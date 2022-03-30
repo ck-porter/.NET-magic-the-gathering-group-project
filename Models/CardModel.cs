4dadf41dff6e9959dc81f6212ed209b7e7ff2339
CK Porter <71106072+ck-porter@users.noreply.github.com>
2022-03-30 11:57:08 -0300

    Add project files.

---
+++ Models/CardModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MTG.Models
+{
+    public class CardModel
+    {
+        public string Name { get; set; }
+
+        public string Image { get; set; }
+
+        public string Type { get; set; }
+
+        public string Power { get; set; }  //attack damage
+
+        public string Toughness { get; set; }  //health points
+
+        public CardModel(string name, string image, string type, string power, string toughness) 
+        {
+            Name = name;
+            Image = image;
+            Type = type;
+            Power = power;  
+            Toughness = toughness;  
+        }
+    }
+}
---
+++ ViewModel/CardViewModel.cs
@@ -0,0 +1,61 @@
+﻿using MTG.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MTG.ViewModel
+{
+    public class CardViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public CardModel _cardModel { get; set; }
+
+        public List<CardModel> _allCards;
+
+        public ObservableCollection<CardModel> Cards { get; set; }
+
+        //for binding
+        public string CardName { get; set; }
+
+        public string CardImage { get; set; }
+
+
+        private CardModel _selectedNote;
+
+        public CardModel SelectedNote
+        {
+            get { return _selectedNote; }
+
+            set
+            {
+                _selectedNote = value;
+
+                if (value == null)
+                {
+                    CardName = "";
+                    CardImage = "";
+                }
+                else
+                {
+                    CardImage = value.Image;
+                    CardName = value.Name;
+                   
+                }
+
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("vmContent"));
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("vmTitle"));
+            }
+        }
+
+        public CardViewModel()
+        {
+            Cards = new ObservableCollection<CardModel>();
+            _allCards = new List<CardModel> ();
+        }
+    }
+}
