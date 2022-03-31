9a9cbc41e6043f71d476a728f37b6e7ab2d0ca07
w0403665 <71242653+w0403665@users.noreply.github.com>
2022-03-30 21:57:18 -0300

    Complete part of Get Data from Api

---
+++ Http/HttpHelper.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MTG.Http
+{
+    public class HttpHelper
+    {
+        static HttpClient client = new HttpClient();
+
+        public static async Task<string> GetXmlAsync(string strRequestUrl)
+        {
+            string temp = "";
+            try
+            {
+                temp = await client.GetStringAsync(strRequestUrl);
+                return temp;
+            }
+            catch { return ""; }
+        }
+
+
+        public static void CancelHttpRequestAsync()
+        {
+            try
+            {
+                client.CancelPendingRequests();
+            }
+            catch { }
+        }
+    }
+}
--- ViewModel/CardViewModel.cs
+++ ViewModel/CardViewModel.cs
@@ -1,9 +1,13 @@
 ﻿using MTG.Models;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -23,6 +27,7 @@ namespace MTG.ViewModel
         public string CardName { get; set; }
 
         public string CardImage { get; set; }
+        public int[] cardsIndex =new int[5];
 
 
         private CardModel _selectedNote;
@@ -56,6 +61,50 @@ namespace MTG.ViewModel
         {
             Cards = new ObservableCollection<CardModel>();
             _allCards = new List<CardModel> ();
+            GetCards();
+            
         }
+
+
+        public async void GetCards()
+        {
+            Uri baseURI = new Uri("https://api.magicthegathering.io/v1/cards/");
+            Uri uri = new Uri(baseURI.ToString());
+            HttpClient httpClient = new HttpClient();
+            var result = await httpClient.GetStringAsync(uri);
+            JObject data = (JObject)JsonConvert.DeserializeObject(result);
+
+            RandomNumbers(data.Count, 1, cardsIndex);
+            for (int i = 0; i < 1; i++)
+            {
+                int index = cardsIndex[i];
+                string name = data["cards"][index]["name"].ToString();
+                string image = data["cards"][index]["imageUrl"].ToString();
+                string type = data["cards"][index]["type"].ToString();
+                string power= data["cards"][index]["power"].ToString();
+                string toughness= data["cards"][index]["toughness"].ToString();
+                CardModel card = new CardModel(name,image,type,power,toughness);
+                _allCards.Add(card);
+                Cards.Add(card);
+            }
+
+        }
+
+
+
+        public void RandomNumbers(int length, int Numbers,int[] cardsIndex)
+        {
+            HashSet<int> cards=new HashSet<int> ();
+            Random rm = new Random();
+
+            for (int i = 0; cards.Count < Numbers; i++)
+            {
+                int nValue = rm.Next(length);
+                cards.Add(nValue);
+            }
+            cards.CopyTo(cardsIndex);
+        
+        }
+
     }
 }
