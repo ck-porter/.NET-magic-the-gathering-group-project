1bd2efbf45ff0db06733785171d851ce7120de31
w0403665 <71242653+w0403665@users.noreply.github.com>
2022-04-04 20:47:10 -0300

    Modified the function of GetCards, and implemented the function of get any number of cards

--- ViewModel/CardViewModel.cs
+++ ViewModel/CardViewModel.cs
@@ -64,12 +64,12 @@ namespace MTG.ViewModel
         {
             Cards = new ObservableCollection<CardModel>();
             _allCards = new List<CardModel> ();
-            GetCards();
+            GetCards(5);
             
         }
 
 
-        public async void GetCards()
+        public async void GetCards(int number)
         {
             Uri baseURI = new Uri("https://api.magicthegathering.io/v1/cards/");
             Uri uri = new Uri(baseURI.ToString());
@@ -77,66 +77,27 @@ namespace MTG.ViewModel
             var result = await httpClient.GetStringAsync(uri);
             JObject data = (JObject)JsonConvert.DeserializeObject(result);
             int count = data["cards"].Count();
-            RandomNumbers(count, 5, cardsIndex);
-
-
-
-
-
-
-            for (int i = 0; i < 5; i++)
-            {
-
-                int index = cardsIndex[i];
-                JToken test = data["cards"][index]["imageUrl"] as JToken;
-                if (test != null) {
-                   
-                        string name = data["cards"][index]["name"].ToString();
-                        string image = data["cards"][index]["imageUrl"].ToString();
-                        CardModel card = new CardModel(name, image);
-                        _allCards.Add(card);
-                        Cards.Add(card);
-
-
-                        //image = image.Replace("&", "&amp;");
-                        //string type = data["cards"][index]["type"].ToString();
-                        //string power = data["cards"][index]["power"].ToString();
-                        //string toughness = data["cards"][index]["toughness"].ToString();
-
-                   
-                }
-
-                //int index = i;
-
-
-
-                //using new model
-
-                //int index = cardsIndex[i];
-                //string name = data["cards"][index]["name"].ToString();
-                //string image = data["cards"][index]["imageUrl"].ToString();
-                //CardModel card = new CardModel(name, image);
-                //_allCards.Add(card);
-                //Cards.Add(card);
-                //}
-
-            }
-        }
-
-
-        public void RandomNumbers(int length, int Numbers,int[] cardsIndex)
-        {
-            HashSet<int> cards=new HashSet<int> ();
+            Random rm = new Random();
             
 
-            for (int i = 0; cards.Count()< Numbers; i++)
-            {
-                Random rm = new Random();
-                int nValue = rm.Next(length);
-                cards.Add(nValue);
-            }
-            cards.CopyTo(cardsIndex);
-        
+            do {
+                int index = rm.Next(count);
+                JToken test = data["cards"][index]["imageUrl"] as JToken;
+                if (test != null)
+                {
+
+                    string name = data["cards"][index]["name"].ToString();
+                    string image = data["cards"][index]["imageUrl"].ToString();
+                    CardModel card = new CardModel(name, image);
+                    _allCards.Add(card);
+                    Cards.Add(card);
+                    number--;
+                }
+                else {
+                  
+                }
+
+                } while (number>0);
         }
 
     }
