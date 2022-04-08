cafbfb0b435590117055ce6c04d66df43d7bbc58
Jade Ye <JadeYe2020>
2022-04-07 21:47:57 -0300

    Yue created CharacterModel class.

---
+++ Models/CharacterModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MTG.Models
+{
+    public class CharacterModel
+    {
+        public string Name { get; set; }
+
+        public string Image { get; set; }
+        public string Type { get; set; }
+
+        public string Power { get; set; }  //attack damage
+
+        public string Toughness { get; set; }  //health points
+
+        public CharacterModel(string name, string image, string type, string power, string toughness)
+        {
+            Name = name;
+            Image = image;
+            Type = type;
+            Power = power;
+            Toughness = toughness;
+        }
+    }
+}
