9f623e7519dc2fcb8fb41dc0c2c7363ce2c26cb5
CK Porter <71106072+ck-porter@users.noreply.github.com>
2022-04-12 14:49:25 -0300

    code cleanup

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -18,9 +18,7 @@ using MTG.View;
 
 namespace MTG
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
+
     public sealed partial class MainPage : Page
     {
         public ViewModel.CardViewModel CardViewModel { get; set; }
